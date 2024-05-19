6b627ea5959eedc4adc719c2b3f219b3239a350a
heisenberg1223 <neversold@protonmail.com>
2024-05-19 14:01:39 +0200

    first commit

--- Exception.hpp
+++ Exception.hpp
@@ -3,22 +3,21 @@
 #ifndef EXCEPTION_HPP
 #define EXCEPTION_HPP
 
-#include "Vlk.hpp"
+#include <exception>
+#include <string>
 
-namespace VlkApp {
-  class Exception : public std::exception {
-    public:
-      Exception(std::string message, const int line_num, std::string file_name);
+class Exception : public std::exception {
+  public:
+    Exception(std::string message, const int line_num, std::string file_name);
 
-      const char* what() const noexcept override;
-      const char* file() const noexcept;
-      int line() const noexcept;
+    const char* what() const noexcept override;
+    const char* file() const noexcept;
+    int line() const noexcept;
 
-    private:
-      const std::string m_message;
-      const std::string m_fileName;
-      const int m_lineNum;
-  };
-}
+  private:
+    const std::string m_message;
+    const std::string m_fileName;
+    const int m_lineNum;
+};
 
 #endif
